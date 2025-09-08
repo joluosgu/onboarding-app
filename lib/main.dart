ae0463a180f01002e3044a7653378079ef0a37e0
joluosgu <joluosgu@gmail.com>
2025-09-08 11:40:04 -0500

    www

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://fwgkjzfxjibgpkbuholf.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3Z2tqemZ4amliZ3BrYnVob2xmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxMDUyMjMsImV4cCI6MjA3MjY4MTIyM30.fecvBRoXuobja15wfaUUj_er6bUPBqU3mXs7UPnY-cw',
+    url: 'https://TU-PROYECTO.supabase.co',
+    anonKey: 'TU-CLAVE-ANONIMA',
   );
 
   runApp(MyApp());
